b830a82a8498c0e745748719549b4f32885561d5
slava.lobikov <lobikova12@mail.ru>
2023-05-08 19:38:23 +0300

    test select

--- src/components/MultySelect/index.jsx
+++ src/components/MultySelect/index.jsx
@@ -5,7 +5,11 @@ const MultySelect = ({options, question, changeCallback}) => {
         {question} (выберите несколько вариантов)
         <Select
             onChange={changeCallback}
+            //closeMenuOnSelect={false}
+            //blurInputOnSelect={true}
+            hideSelectedOptions={true}
             closeMenuOnSelect={false}
+            blurInputOnSelect={false}
             placeholder='Выберите несколько вариантов'
             isMulti
             options={options}
